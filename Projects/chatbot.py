1e223e660a5af4c4f66188ad0cd132ac4a9acf4c
Abhyudai <itsabhyudai@gmail.com>
2026-05-26 16:15:29 +0530

    added prompts part

---
+++ Projects/chatbot.py
@@ -0,0 +1,29 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V4-Flash",
+    task="text-generation",
+    temperature=0.5
+)
+
+model = ChatHuggingFace(llm=llm)
+
+chat_history = [
+    SystemMessage(content="You are a helpful AI assistant"),
+]
+
+while True:
+    user_input = input("You: ")
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == "exit":
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ", result.content)
+
+print(chat_history)
+
